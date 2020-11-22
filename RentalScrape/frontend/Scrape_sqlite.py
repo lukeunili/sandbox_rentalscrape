8db671a92193ae45d6d3477b785d12d93dbbd810
Johannes Schleicher <johannes.schleicher@uni.li>
2020-11-22 02:35:29 +0100

    Added pictures, added a loop to the scrape File

--- RentalScrape/frontend/Scrape_sqlite.py
+++ RentalScrape/frontend/Scrape_sqlite.py
@@ -1,9 +1,18 @@
-from pandas import DataFrame
+#from typing import List
+
+#from pandas import DataFrame
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
 import pandas as pd
 import sqlite3 as sql
+
+#gettingvalues = sql.connect('../db.sqlite3')
+#gettingvaluescursor = gettingvalues.cursor()
+#pickupstationsearch = gettingvaluescursor.execute("select 'Station' from 'frontend_searchinput' order by id desc limit 1")
+
+#print(pickupstationsearch)
+
 
 # specify the url
 urlpage = 'https://www.sixt.at/'
@@ -12,13 +21,16 @@ print(urlpage)
 # define chromedriver to execute headless, incl. window size
 WINDOW_SIZE = "1920,1080"
 chrome_options = Options()
-chrome_options.add_argument("--headless")
+#chrome_options.add_argument("--headless")
 chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
 
 # run Chrome webdriver from executable path of your choice
 driver = webdriver.Chrome(options=chrome_options)
 
 #time.sleep(2)
+
+SearchPickUpTime = ['14:00', '14:30', '15:00']
+SearchDropOffTime = ['8:00', '8:30', '9:00', '9:30']
 
 # get web page
 driver.get(urlpage)
@@ -64,7 +76,7 @@ time.sleep(0.5)
 rental_PickUpTimeButton = driver.find_element_by_xpath("//*[@class='SearchEngine__pickupDateTime']//*[@class='TimeButton__horizontal TimeButton__wrapper']")
 rental_PickUpTimeButton.click()
 time.sleep(0.5)
-rental_PickUpTime = driver.find_element_by_xpath("//*[@id='root']/div/div[1]/div[2]/div[1]/div/div/div/div[3]/div/span/div/div/div[2]/div/div/div[29]")
+rental_PickUpTime = driver.find_element_by_xpath("//*[contains(text(), '13:30')]")
 rental_PickUpTime.click()
 
 time.sleep(0.5)
@@ -74,7 +86,7 @@ time.sleep(0.5)
 rental_DropOffTimeButton = driver.find_element_by_xpath("//*[@class='SearchEngine__returnDateTime']//*[@class='TimeButton__horizontal TimeButton__wrapper']")
 rental_DropOffTimeButton.click()
 time.sleep(0.5)
-rental_DropOffTime = driver.find_element_by_xpath("//*[@id='root']/div/div[1]/div[2]/div[1]/div/div/div/div[3]/div/span/div/div/div[2]/div/div/div[25]")
+rental_DropOffTime = driver.find_element_by_xpath("//*[contains(text(), '9:00')]")
 rental_DropOffTime.click()
 
 # confirm the times and dates
@@ -99,6 +111,9 @@ for result in offers:
     car_type_element = result.find_element_by_class_name("OfferTile__descriptionTitle")
     car_type = car_type_element.text
 
+    car_description_element = result.find_element_by_class_name("OfferTile__descriptionText")
+    car_description = car_description_element.text
+
     car_price_element = result.find_element_by_class_name("OfferTile__offerPriceNormal")
     car_price = car_price_element.text
 
@@ -121,66 +136,129 @@ for result in offers:
     bookingclass_element = result.find_element_by_class_name("OfferTile__wrapper")
     bookingclass = bookingclass_element.get_attribute("class")
 
-    cardescription_element = xx
-    cardescription = xx
 
     # append dict to array
-    data.append({"cartype": car_type, "price": car_price[1:-6],"mileage": mileage, "pickupdate" : pickup_date, "pickuptime" : pickup_time, "dropoffdate" : dropoff_date, "dropofftime" : dropoff_time, "bookingclass": bookingclass[-4:], "cardescription": cardescription})
+    data.append({"cartype": car_type, "cardescription": car_description, "price": car_price[1:-6],"mileage": mileage, "pickupdate" : pickup_date, "pickuptime" : pickup_time, "dropoffdate" : dropoff_date, "dropofftime" : dropoff_time, "bookingclass": bookingclass[-4:]})
 
 df = pd.DataFrame(data)
 #df.to_csv('firsttry.csv')
 
 
-time.sleep(2)
+for SearchPickUpTime in SearchPickUpTime:
+    driver.execute_script("window.scrollTo(0, 0);")
+    rental_PickUpTimeButton = driver.find_element_by_xpath("//*[@class='SearchEngine__pickupDateTime']//*[@class='TimeButton__horizontal TimeButton__wrapper']")
+    rental_PickUpTimeButton.click()
+    time.sleep(0.5)
+    #rental_PickUpTimeSearch = """ "//*[contains(text(), '""" + str(SearchPickUpTime) + """')]" """
+    rental_PickUpTime1 = driver.find_element_by_xpath("//*[contains(text(), '" + (SearchPickUpTime) +"')]")
+    rental_PickUpTime1.click()
+    time.sleep(1)
 
-# choose pick up time 14:30
-rental_PickUpTimeButton = driver.find_element_by_xpath("//*[@class='SearchEngine__pickupDateTime']//*[@class='TimeButton__horizontal TimeButton__wrapper']")
-rental_PickUpTimeButton.click()
-time.sleep(0.5)
-rental_PickUpTime = driver.find_element_by_xpath("//*[@id='root']/div/div[1]/div[2]/div[1]/div/div/div/div[2]/div/span/div/div/div[2]/div/div/div[30]")
-rental_PickUpTime.click()
+    # scroll until the end of the website
+    driver.execute_script(
+        "window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
 
-time.sleep(1)
+    time.sleep(2)
+    offers3 = driver.find_elements_by_xpath("//*[@class='OfferList__gridItem']")
 
-# scroll until the end of the website
-driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+    for result in offers3:
+        car_type_element = result.find_element_by_class_name("OfferTile__descriptionTitle")
+        car_type = car_type_element.text
 
-time.sleep(1)
+        car_description_element = result.find_element_by_class_name("OfferTile__descriptionText")
+        car_description = car_description_element.text
 
-# defines all possible rental offers at the specific station and certain dates
-offers2 = driver.find_elements_by_xpath("//*[@class='OfferList__gridItem']")
-print('Number of results', len(offers2))
+        car_price_element = result.find_element_by_class_name("OfferTile__offerPriceNormal")
+        car_price = car_price_element.text
 
-for result in offers2:
+        mileage_element = result.find_element_by_class_name("CheckList__checkmarkTitle")
+        mileage = mileage_element.text
 
-    car_type_element = result.find_element_by_class_name("OfferTile__descriptionTitle")
-    car_type = car_type_element.text
+        pickup_date_element = result.find_element_by_xpath(
+            "/html/body/div/div[1]/div[2]/div/div/div[1]/div[2]/div[1]/div/div/div/div[1]/div[1]/div[2]/div[1]/div/div[1]/div/span")
+        pickup_date = pickup_date_element.text
 
-    car_price_element = result.find_element_by_class_name("OfferTile__offerPriceNormal")
-    car_price = car_price_element.text
+        pickup_time_element = result.find_element_by_xpath(
+            "/html/body/div/div[1]/div[2]/div/div/div[1]/div[2]/div[1]/div/div/div/div[1]/div[1]/div[2]/div[1]/div/div[2]/div/span[1]")
+        pickup_time = pickup_time_element.text
 
-    mileage_element = result.find_element_by_class_name("CheckList__checkmarkTitle")
-    mileage = mileage_element.text
+        dropoff_date_element = result.find_element_by_xpath(
+            "/html/body/div/div[1]/div[2]/div/div/div[1]/div[2]/div[1]/div/div/div/div[1]/div[1]/div[2]/div[2]/div/div[1]/div/span")
+        dropoff_date = dropoff_date_element.text
 
-    pickup_date_element = result.find_element_by_xpath("/html/body/div/div[1]/div[2]/div/div/div[1]/div[2]/div[1]/div/div/div/div[1]/div[1]/div[2]/div[1]/div/div[1]/div/span")
-    pickup_date = pickup_date_element.text
+        dropoff_time_element = result.find_element_by_xpath(
+            "/html/body/div/div[1]/div[2]/div/div/div[1]/div[2]/div[1]/div/div/div/div[1]/div[1]/div[2]/div[2]/div/div[2]/div/span[1]")
+        dropoff_time = dropoff_time_element.text
 
-    pickup_time_element = result.find_element_by_xpath("/html/body/div/div[1]/div[2]/div/div/div[1]/div[2]/div[1]/div/div/div/div[1]/div[1]/div[2]/div[1]/div/div[2]/div/span[1]")
-    pickup_time = pickup_time_element.text
+        bookingclass_element = result.find_element_by_class_name("OfferTile__wrapper")
+        bookingclass = bookingclass_element.get_attribute("class")
 
-    dropoff_date_element = result.find_element_by_xpath("/html/body/div/div[1]/div[2]/div/div/div[1]/div[2]/div[1]/div/div/div/div[1]/div[1]/div[2]/div[2]/div/div[1]/div/span")
-    dropoff_date = dropoff_date_element.text
+        data.append(
+            {"cartype": car_type, "cardescription": car_description, "price": car_price[1:-6], "mileage": mileage,
+             "pickupdate": pickup_date, "pickuptime": pickup_time, "dropoffdate": dropoff_date,
+             "dropofftime": dropoff_time, "bookingclass": bookingclass[-4:]})
+        df = pd.DataFrame(data)
+    print("Sucess: ", SearchPickUpTime, SearchDropOffTime)
 
-    dropoff_time_element = result.find_element_by_xpath("/html/body/div/div[1]/div[2]/div/div/div[1]/div[2]/div[1]/div/div/div/div[1]/div[1]/div[2]/div[2]/div/div[2]/div/span[1]")
-    dropoff_time = dropoff_time_element.text
+    for SearchDropOffTime in SearchDropOffTime:
+        time.sleep(1)
+        driver.execute_script("window.scrollTo(0, 0);")
+        rental_DropOffTimeButton2 = driver.find_element_by_xpath("//*[@class='SearchEngine__returnDateTime']//*[@class='TimeButton__horizontal TimeButton__wrapper']")
+        rental_DropOffTimeButton2.click()
+        time.sleep(0.5)
+            # rental_PickUpTimeSearch = """ "//*[contains(text(), '""" + str(SearchPickUpTime) + """')]" """
+        rental_DropoffTime2 = driver.find_element_by_xpath("//*[contains(text(), '" + str(SearchDropOffTime) +"')]")
+        print(SearchDropOffTime)
+        rental_DropoffTime2.click()
 
-    bookingclass_element = result.find_element_by_class_name("OfferTile__wrapper")
-    bookingclass = bookingclass_element.get_attribute("class")
 
-    cardescription_element = xx
-    cardescription = xx
 
-    data.append({"cartype": car_type, "price": car_price[1:-6], "mileage": mileage, "pickupdate": pickup_date, "pickuptime": pickup_time, "dropoffdate": dropoff_date, "dropofftime": dropoff_time, "bookingclass": bookingclass[-4:], "cardescription": cardescription})
+        time.sleep(1)
+
+        # scroll until the end of the website
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+
+        time.sleep(1)
+        offers4 = driver.find_elements_by_xpath("//*[@class='OfferList__gridItem']")
+        for result in offers4:
+            car_type_element = result.find_element_by_class_name("OfferTile__descriptionTitle")
+            car_type = car_type_element.text
+
+            car_description_element = result.find_element_by_class_name("OfferTile__descriptionText")
+            car_description = car_description_element.text
+
+            car_price_element = result.find_element_by_class_name("OfferTile__offerPriceNormal")
+            car_price = car_price_element.text
+
+            mileage_element = result.find_element_by_class_name("CheckList__checkmarkTitle")
+            mileage = mileage_element.text
+
+            pickup_date_element = result.find_element_by_xpath(
+                    "/html/body/div/div[1]/div[2]/div/div/div[1]/div[2]/div[1]/div/div/div/div[1]/div[1]/div[2]/div[1]/div/div[1]/div/span")
+            pickup_date = pickup_date_element.text
+
+            pickup_time_element = result.find_element_by_xpath(
+                    "/html/body/div/div[1]/div[2]/div/div/div[1]/div[2]/div[1]/div/div/div/div[1]/div[1]/div[2]/div[1]/div/div[2]/div/span[1]")
+            pickup_time = pickup_time_element.text
+
+            dropoff_date_element = result.find_element_by_xpath(
+                    "/html/body/div/div[1]/div[2]/div/div/div[1]/div[2]/div[1]/div/div/div/div[1]/div[1]/div[2]/div[2]/div/div[1]/div/span")
+            dropoff_date = dropoff_date_element.text
+
+            dropoff_time_element = result.find_element_by_xpath(
+                    "/html/body/div/div[1]/div[2]/div/div/div[1]/div[2]/div[1]/div/div/div/div[1]/div[1]/div[2]/div[2]/div/div[2]/div/span[1]")
+            dropoff_time = dropoff_time_element.text
+
+            bookingclass_element = result.find_element_by_class_name("OfferTile__wrapper")
+            bookingclass = bookingclass_element.get_attribute("class")
+
+            data.append(
+                    {"cartype": car_type, "cardescription": car_description, "price": car_price[1:-6], "mileage": mileage,
+                     "pickupdate": pickup_date, "pickuptime": pickup_time, "dropoffdate": dropoff_date,
+                     "dropofftime": dropoff_time, "bookingclass": bookingclass[-4:]})
+            df = pd.DataFrame(data)
+        print("Sucess:", SearchPickUpTime, SearchDropOffTime)
+
 
 df = pd.DataFrame(data)
 df.sort_values(by='price', ascending=True, ignore_index=True)
